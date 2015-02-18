7002304cf3eafb013c83e7515119c6ce9bac5bb8
Alex Gisby <alex@solution10.com>
2015-02-18 07:32:23 +0000

    Directories command added.

---
+++ src/DirectoriesCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Solution10\Devkit;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DirectoriesCommand extends DevkitCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('directories')
+            ->setDescription('Creates docs, src and tests directories in the project')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $baseDir = $this->containerDirectory();
+        $toCreate = ['docs', 'src', 'tests'];
+        foreach ($toCreate as $folder) {
+            $fullpath = $baseDir.DIRECTORY_SEPARATOR.$folder;
+            if (!file_exists($fullpath)) {
+                mkdir($fullpath);
+                $output->writeln('<info>Created directory '.$fullpath.'</info>');
+            } else {
+                $output->writeln('<error>Directory '.$fullpath.' already exists!</error>');
+            }
+        }
+    }
+}
